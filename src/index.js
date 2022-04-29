aab5d0fb41bf86fe14d4b9010486747b1322f204
octorose <allouchhatim@gmail.com>
2022-04-29 02:50:49 +0000

    GTM

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import ReactDOM  from "react-dom";
 import React from "react";
 import App from "./App";
 import './index.css';
+import TagManager from 'react-gtm-module'
 
+const tagManagerArgs = {
+    gtmId: 'GTM-KCQG27M'
+}
+
+TagManager.initialize(tagManagerArgs)
 
 ReactDOM.render(<App/>, document.getElementById('root'));
